4cfe9619227428cb97eba14ce51646fdb3751739
Hannah Park <hannahpark@fb.com>
2018-07-17 16:38:23 -0700

    master can control volume using seek arc

--- app/src/main/java/com/example/bertogonz3000/songtest/SeekArcActivity.java
+++ app/src/main/java/com/example/bertogonz3000/songtest/SeekArcActivity.java
@@ -1,35 +1,48 @@
 package com.example.bertogonz3000.songtest;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.sdsmdg.harjot.crollerTest.Croller;
 import com.sdsmdg.harjot.crollerTest.OnCrollerChangeListener;
-
 public class SeekArcActivity extends AppCompatActivity {
+    private AudioManager audioManager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seek_arc);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
 
         Croller croller = (Croller) findViewById(R.id.croller);
         croller.setIndicatorWidth(10);
         croller.setBackCircleColor(Color.parseColor("#EDEDED"));
         croller.setMainCircleColor(Color.WHITE);
-        croller.setMax(50);
+        //croller.setMax(50);
         croller.setStartOffset(45);
         croller.setIsContinuous(false);
+        croller.setLabel("Surround");
         croller.setLabelColor(Color.BLACK);
         croller.setProgressPrimaryColor(Color.parseColor("#0B3C49"));
         croller.setIndicatorColor(Color.parseColor("#0B3C49"));
         croller.setProgressSecondaryColor(Color.parseColor("#EEEEEE"));
 
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        croller.setMax(audioManager
+                .getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+        croller.setProgress(audioManager
+                .getStreamVolume(AudioManager.STREAM_MUSIC));
+
         croller.setOnCrollerChangeListener(new OnCrollerChangeListener() {
             @Override
             public void onProgressChanged(Croller croller, int progress) {
                 // use the progress
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                        progress, 0);
             }
 
             @Override
